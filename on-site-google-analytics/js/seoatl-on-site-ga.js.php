3dd5ee0d24bbde2f8a6de97863d12f28be564130
james.charlesworth <james.charlesworth@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-13 00:59:43 +0000

    Fixed path issues
    git-svn-id: https://plugins.svn.wordpress.org/on-site-google-analytics/trunk@263073 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- on-site-google-analytics/js/seoatl-on-site-ga.js.php
+++ on-site-google-analytics/js/seoatl-on-site-ga.js.php
@@ -6,7 +6,7 @@ if (!function_exists('add_action'))
 }
 ?>
 jQuery(document).ready(function(){
-    var url = "<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/php/seoatl-onsite-ga-ajax.php";
+    var url = "<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/php/seoatl-onsite-ga-ajax.php";
     jQuery("#onsite-ga-plugin-inner").load(url,{'request_uri': jQuery("#seoatl_onsite_ga_request_uri").val()});
 
  
--- on-site-google-analytics/seoatl-onsite-ga.php
+++ on-site-google-analytics/seoatl-onsite-ga.php
@@ -113,7 +113,7 @@ if (!class_exists("SeoatlOnSiteGa")) {
                         <select id="seoatlGaProfileId" name="seoatlGaProfileId">
                             <option value=""></option>
                         </select>
-                        <img id="profile_loader" style="display:none;" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/images/admin-loader.gif" />
+                        <img id="profile_loader" style="display:none;" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/images/admin-loader.gif" />
 
                         <br /><br />
                        <label for="seoatlGaProfileId">Date Range:</label>
@@ -134,14 +134,14 @@ if (!class_exists("SeoatlOnSiteGa")) {
                           
                             jQuery("#seoatlGaPassword").bind("blur",function(){
                                 jQuery("#profile_loader").css("display","inline");
-                               jQuery("#seoatlGaProfileId").load("<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/php/ga-profile-ajax.php",{'username': jQuery("#seoatlGaUsername").val(),'password': jQuery("#seoatlGaPassword").val()}, function(){
+                               jQuery("#seoatlGaProfileId").load("<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/php/ga-profile-ajax.php",{'username': jQuery("#seoatlGaUsername").val(),'password': jQuery("#seoatlGaPassword").val()}, function(){
                                   jQuery("#profile_loader").css("display","none");
                                });
                             })
 
                             jQuery(document).ready(function(){
                                  jQuery("#profile_loader").css("display","inline");
-                                jQuery("#seoatlGaProfileId").load("<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/php/ga-profile-ajax.php",{'username': jQuery("#seoatlGaUsername").val(),'password': jQuery("#seoatlGaPassword").val()},function(){
+                                jQuery("#seoatlGaProfileId").load("<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/php/ga-profile-ajax.php",{'username': jQuery("#seoatlGaUsername").val(),'password': jQuery("#seoatlGaPassword").val()},function(){
                                     jQuery("#profile_loader").css("display","none");
                                 });
                             })
@@ -188,12 +188,12 @@ if (!class_exists("SeoatlOnSiteGa")) {
                      if ($user_info->user_level!=10) {
                          return;
                      }
-                     echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/seoatl-onsite-ga/css/seoatl-on-site-ga.css" />' . "\n";
-                     echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/seoatl-onsite-ga/css/tipsy.css" />' . "\n";
+                     echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/on-site-google-analytics/css/seoatl-on-site-ga.css" />' . "\n";
+                     echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/on-site-google-analytics/css/tipsy.css" />' . "\n";
                     if (function_exists('wp_enqueue_script')) {
-                        wp_enqueue_script('seoatl_on_site_ga_c', get_bloginfo('wpurl') . '/wp-content/plugins/seoatl-onsite-ga/js/jquery-ui-1.8.2.custom.min.js', array('jquery'), '0.1');
-                        wp_enqueue_script('seoatl_on_site_ga_b', get_bloginfo('wpurl') . '/wp-content/plugins/seoatl-onsite-ga/js/jquery.tipsy.js', array('jquery'), '0.1');
-                        wp_enqueue_script('seoatl_on_site_ga_a', get_bloginfo('wpurl') . '/wp-content/plugins/seoatl-onsite-ga/js/seoatl-on-site-ga.js.php', array('jquery'), '0.1');
+                        wp_enqueue_script('seoatl_on_site_ga_c', get_bloginfo('wpurl') . '/wp-content/plugins/on-site-google-analytics/js/jquery-ui-1.8.2.custom.min.js', array('jquery'), '0.1');
+                        wp_enqueue_script('seoatl_on_site_ga_b', get_bloginfo('wpurl') . '/wp-content/plugins/on-site-google-analytics/js/jquery.tipsy.js', array('jquery'), '0.1');
+                        wp_enqueue_script('seoatl_on_site_ga_a', get_bloginfo('wpurl') . '/wp-content/plugins/on-site-google-analytics/js/seoatl-on-site-ga.js.php', array('jquery'), '0.1');
                         
                     }
 
@@ -220,7 +220,7 @@ if (!class_exists("SeoatlOnSiteGa")) {
                ?>
                  
                         <div id="onsite-ga-plugin" style="position:fixed;"><div id="onsite-ga-plugin-inner">
-                     <img src="<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/images/ajax-loader.gif" />
+                     <img src="<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/images/ajax-loader.gif" />
                      <input type="hidden" id="seoatl_onsite_ga_request_uri" name="seoatl_onsite_ga_request_uri" value="<?php echo $_SERVER['REQUEST_URI'];?>" />
                 </div></div><br /><br />
                 
@@ -322,7 +322,7 @@ if (!class_exists("SeoatlOnSiteGa")) {
                      unset($ga);
                      $ga = new gapi($seoatlOptions['ga_username'],$seoatlOptions['ga_password']);
                      $keyword_filter = "ga:keyword!=(not set) && pagePath==".$request_uri;
-                     $ga->requestReportData($seoatlOptions['ga_profile_id'],array('keyword','source','medium','date'),array('visits'),'-date',$keyword_filter,$start_date,$end_date);
+                     $ga->requestReportData($seoatlOptions['ga_profile_id'],array('keyword','source','medium'),array('visits'),'-visits',$keyword_filter,$start_date,$end_date);
                      $keywords = array();
                      foreach($ga->getResults() as $result)
                      {
@@ -345,11 +345,11 @@ if (!class_exists("SeoatlOnSiteGa")) {
                      $total_visits = $ga->getVisits();
                      ?>
                      
-                     <div class="onsite-ga-stat"><span class="label">Views: <?php echo $views; ?></span><a href="#" onclick="return false" title="The number of times this page has been viewed by website visitors <?php echo $tip_date; ?>." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/images/question.png" /></a></div>
-                     <div class="onsite-ga-stat"><span class="label">Avg Time on Page: <?php echo $avg_time;?></span><a href="#" onclick="return false" title="The average amount of time visitors spent on this page" class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/images/question.png" /></a></div>
+                     <div class="onsite-ga-stat"><span class="label">Views: <?php echo $views; ?></span><a href="#" onclick="return false" title="The number of times this page has been viewed by website visitors <?php echo $tip_date; ?>." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/images/question.png" /></a></div>
+                     <div class="onsite-ga-stat"><span class="label">Avg Time on Page: <?php echo $avg_time;?></span><a href="#" onclick="return false" title="The average amount of time visitors spent on this page" class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/images/question.png" /></a></div>
 
-                     <div class="onsite-ga-stat"><span class="label">Bounce Rate:  <?php echo $bounce_rate; ?>%</span><a href="#" onclick="return false" title="A bounce is determined by a single page visit to your site. The bounce rate is the number of bounces divided by the number of entrances." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/images/question.png" /></a></div>
-                     <div class="onsite-ga-stat"><span class="label">Exit Rate: <?php echo $exit_rate; ?>%</span><a href="#" onclick="return false" title="The number of visitors who exited from your site on this page. The exit rate is calcuated by the number of exits divided by the number of page views." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/images/question.png" /></a></div>
+                     <div class="onsite-ga-stat"><span class="label">Bounce Rate:  <?php echo $bounce_rate; ?>%</span><a href="#" onclick="return false" title="A bounce is determined by a single page visit to your site. The bounce rate is the number of bounces divided by the number of entrances." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/images/question.png" /></a></div>
+                     <div class="onsite-ga-stat"><span class="label">Exit Rate: <?php echo $exit_rate; ?>%</span><a href="#" onclick="return false" title="The number of visitors who exited from your site on this page. The exit rate is calcuated by the number of exits divided by the number of page views." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/images/question.png" /></a></div>
                      <div class="onsite-ga-stat onsite-ga-hover" id="onsite-ga-referring-sites">
                          <?php if (count($referring_visits)>0) :?>
                          <div class="onsite-ga-referring-sites">
@@ -361,7 +361,7 @@ if (!class_exists("SeoatlOnSiteGa")) {
 
                          </div>
                          <?php endif; ?>
-                        <span class="label"><?php echo count($referring_visits); ?> Referring Sites Sent <?php echo $r_total_visits; ?> Visits</span><a href="#" onclick="return false" title="The 30 most recent websites that are currently driving traffic to your website via links <?php echo $tip_date;?>." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/images/question.png" /></a></div>
+                        <span class="label"><?php echo count($referring_visits); ?> Referring Sites Sent <?php echo $r_total_visits; ?> Visits</span><a href="#" onclick="return false" title="The 30 most recent websites that are currently driving traffic to your website via links <?php echo $tip_date;?>." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/images/question.png" /></a></div>
                      <div class="onsite-ga-stat onsite-ga-hover onsite-ga-stat-last" id="onsite-ga-keywords">
                          <?php if (count($keywords)>0) :?>
                          <div class="onsite-ga-keywords">
@@ -431,7 +431,7 @@ if (!class_exists("SeoatlOnSiteGa")) {
 	});
 	</script>
 
-                        <span class="label"><?php echo count($keywords); ?> Keywords Sent <?php echo $total_visits; ?> Visits</span> <a href="#" onclick="return false" title="The 30 most recent terms search engine visitors are using to find this page <?php echo $tip_date; ?>." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/seoatl-onsite-ga/images/question.png" /></a></div>
+                        <span class="label"><?php echo count($keywords); ?> Keywords Sent <?php echo $total_visits; ?> Visits</span> <a href="#" onclick="return false" title="The 30 most recent terms search engine visitors are using to find this page <?php echo $tip_date; ?>." class="tipTip"><img width="12" height="12" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/on-site-google-analytics/images/question.png" /></a></div>
                <script type="text/javascript">
                      jQuery(function() {
                        jQuery('.tipTip').tipsy({gravity: 's'});
@@ -477,7 +477,7 @@ if (isset($seoatl_onsite_ga_plugin)) {
     	add_action('wp_head', array(&$seoatl_onsite_ga_plugin, 'addHeaderCode'), 1);
 	add_action('wp_footer', array(&$seoatl_onsite_ga_plugin, 'addFooterCode'), 1);
         add_action('admin_menu', 'SeoatlOnSiteGa_ap');
-        add_action('activate_seoatl-onsite-ga/seoatl-onsite-ga.php', array(&$seoatl_onsite_ga_plugin, 'init'));
+        add_action('activate_on-site-google-analytics/seoatl-onsite-ga.php', array(&$seoatl_onsite_ga_plugin, 'init'));
          
 
         ////Filters
